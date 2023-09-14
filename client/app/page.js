9aae4d23121d45c6d77d6036a4901fbf5fe14fe3
Romain DEROCLE <romain.derocle-ext@gsoi.fr>
2023-09-14 15:56:50 +0200

    wip

--- client/app/page.js
+++ client/app/page.js
@@ -8,16 +8,8 @@ import { ToastContainer, toast } from 'react-toastify';
 export default function Home() {
   return (
     <>
-      <Row className=" pe-3 vh-100 overflow-hidden  g-0">
-        <Col className="side-bar-col" lg={3} xs={3}>
-          <div>
-            <div className="d-flex align-items-center justify-content-center py-4">
-              <h3>PrivateGPT</h3>
-            </div>
-            <ConfigSideNav />
-          </div>
-        </Col>
-        <Col lg={9} xs={9} className="main-chat-col mt-3">
+      <Row className=" p-3 vh-100 overflow-hidden  g-0">
+        <Col lg={12} xs={12} className="main-chat-col">
           <MainContainer />
         </Col>
         <ToastContainer />
